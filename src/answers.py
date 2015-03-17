6dfdcd4828f13eef0ce199dd51e8d4f035d8573a
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2015-03-17 12:53:48 +0100

    Update after discussion with the professors

--- src/answers.py
+++ src/answers.py
@@ -2,11 +2,11 @@ import mlbd
 import pylab as pl
 from matplotlib import gridspec
 
-def curvature_hist( img, step=10, plot=False ):
+def curvature_hist( img, step=10, plot=False, min=0, max=100 ):
 	gs = pl.GridSpec( 2, 2 )
 	c = mlbd.curvature( img, step, plot, gs )
 	if plot :
 		pl.subplot( gs[1,:] )
 		pl.title( 'histogram of curvatures' )
 		pl.hist( c )
-	return pl.histogram( c )[1]
+	return pl.histogram( c )[0]
